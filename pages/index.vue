d519281de44d28aaf02e37dfac3769417c207afa
Raymond Chai <csechuan@gmail.com>
2021-06-29 14:44:22 +0800

    chore: scaffold store

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,6 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  // middleware: 'redirect',
   mounted() {
     window.console.log(this.$nuxt)
   },
---
+++ store/user.js
@@ -0,0 +1,23 @@
+const state = () => ({
+  value: 'myvalue',
+})
+
+const getters = {
+  getterValue: (state) => {
+    return state.value
+  },
+}
+
+const mutations = {
+  updateValue: (state, payload) => {
+    state.value = payload
+  },
+}
+
+const actions = {
+  updateActionValue({ commit }) {
+    commit('updateValue', payload)
+  },
+}
+
+export default { state, getters, actions, mutations }
