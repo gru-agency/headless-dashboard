7b548d99cb50c8264b680068859ed1dcd4171c14
Raymond Chai <csechuan@gmail.com>
2021-06-29 14:09:40 +0800

    chore: scaffold layout folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  mounted() {
+    window.console.log(this.$nuxt)
+  },
+})
 </script>
