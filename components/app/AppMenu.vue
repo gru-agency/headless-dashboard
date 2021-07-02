8cf203e550dbffdf7bb3f0c2efca635480e86e72
Raymond Chai <csechuan@gmail.com>
2021-07-02 22:35:23 +0800

    feat(component): add AppMenu component

---
+++ components/app/AppMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-list-group flush>
+    <b-list-group-item class="navbar-h d-flex align-items-center h3 mb-0 font-weight-bold border-0">
+      shoplex
+    </b-list-group-item>
+    <b-list-group-item v-for="i in 10" :key="i" class="border-0">
+      Cras justo odio {{ i }}
+    </b-list-group-item>
+  </b-list-group>
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+}
+</script>
