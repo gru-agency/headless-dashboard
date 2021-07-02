3c53d9d19ad394b4ca96be73a136ccdd627679e8
Raymond Chai <csechuan@gmail.com>
2021-07-02 22:32:56 +0800

    feat(component): add sidebar component

---
+++ components/lib/bootstrap/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-collapse
+    :id="id"
+    :class="{ 'border-right': rightBorder }"
+    is-nav
+    tag="aside"
+    class="cb-sidebar d-lg-block z-dropdown of-y-auto"
+  >
+    <div class="pb-5">
+      <slot></slot>
+    </div>
+  </b-collapse>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+
+  props: {
+    id: { type: String, default: undefined },
+    rightBorder: { type: Boolean, default: false },
+  },
+}
+</script>
