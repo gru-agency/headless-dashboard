94d087cf67c54225365d4328c833fda34a70aecf
Raymond Chai <csechuan@gmail.com>
2021-07-05 01:40:59 +0800

    refactor(icon): migrate from bs-icon to fa-icon

--- components/app/AppMenu.vue
+++ components/app/AppMenu.vue
@@ -3,57 +3,53 @@
     <b-nav-text class="navbar-h d-flex align-items-center h4 font-weight-bold px-4">
       {{ $app.brandName.toLowerCase() }}
     </b-nav-text>
+
     <b-nav-item :to="links.home" :exact-active-class="activeClass" class="px-2">
-      <b-icon-house-door :class="iconPaddingClass"></b-icon-house-door> {{ home }}
+      <fa :icon="['fad', 'home-alt']" :class="iconClass"></fa> {{ home }}
     </b-nav-item>
+
     <b-nav-item
       :to="links.orders"
       :active-class="activeClass"
       :exact-active-class="activeClass"
       class="px-2"
     >
-      <b-icon-files :class="iconPaddingClass"></b-icon-files> {{ orders }}
+      <fa :icon="['fad', 'ballot']" :class="iconClass"></fa> {{ orders }}
     </b-nav-item>
+
     <b-nav-item
       :to="links.products"
       :active-class="activeClass"
       :exact-active-class="activeClass"
       class="px-2"
     >
-      <b-icon-box-seam :class="iconPaddingClass"></b-icon-box-seam> {{ products }}
+      <fa :icon="['fad', 'box']" :class="iconClass"></fa>
+      {{ products }}
     </b-nav-item>
+
     <b-nav-item
       :to="links.prices"
       :active-class="activeClass"
       :exact-active-class="activeClass"
       class="px-2"
     >
-      <b-icon-cash :class="iconPaddingClass"></b-icon-cash> {{ prices }}
+      <fa :icon="['fad', 'dollar-sign']" :class="iconClass"></fa> {{ prices }}
     </b-nav-item>
+
     <b-nav-item
       :to="links.settings"
       :active-class="activeClass"
       :exact-active-class="activeClass"
       class="px-2"
     >
-      <b-icon-gear :class="iconPaddingClass"></b-icon-gear> {{ settings }}
+      <fa :icon="['fad', 'cog']" :class="iconClass"></fa> {{ settings }}
     </b-nav-item>
   </b-nav>
 </template>
 
 <script>
-import { BIconHouseDoor, BIconFiles, BIconBoxSeam, BIconCash, BIconGear } from 'bootstrap-vue'
-
 export default {
   name: 'AppMenu',
-
-  components: {
-    BIconHouseDoor,
-    BIconBoxSeam,
-    BIconFiles,
-    BIconCash,
-    BIconGear,
-  },
 
   data() {
     return {
@@ -73,8 +69,12 @@ export default {
   },
 
   computed: {
+    iconClass() {
+      return 'mr-2 fa-fw fa-gru'
+    },
+
     iconPaddingClass() {
-      return 'mr-3'
+      return 'mr-2'
     },
 
     activeClass() {
