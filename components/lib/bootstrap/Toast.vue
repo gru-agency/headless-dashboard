cbfd715e4f0f89290119f186bb92a70b6c3b8848
Raymond Chai <csechuan@gmail.com>
2021-07-06 20:11:18 +0800

    feat(toast): add static prop + fix id bug

--- components/lib/bootstrap/Toast.vue
+++ components/lib/bootstrap/Toast.vue
@@ -1,10 +1,11 @@
 <template>
   <b-toast
-    id="bvBottomCenter"
+    :id="id"
     toaster="b-toaster-bottom-center"
     :variant="getVariant"
     :no-auto-hide="noAutoHide"
     :auto-hide-delay="autoHideDelay"
+    :static="isStatic"
     :solid="solid"
   >
     <template #toast-title>
@@ -26,6 +27,7 @@ export default {
     message: { type: String, default: undefined },
     variant: { type: String, default: undefined },
     solid: { type: Boolean, default: true },
+    isStatic: { type: Boolean, default: false },
     noAutoHide: { type: Boolean, default: false },
     autoHideDelay: { type: [Number, String], default: 8000 },
     icon: { type: Array, default: () => null },
