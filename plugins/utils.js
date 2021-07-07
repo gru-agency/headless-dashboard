d97865cdc20ac63ff0d11a3c039f5bbb0d50ab0e
Raymond Chai <csechuan@gmail.com>
2021-07-07 21:49:35 +0800

    refactor(utils): remove unused functions in utils

--- plugins/utils.js
+++ plugins/utils.js
@@ -13,33 +13,15 @@ const ui = {
 
 const app = {
   brandName: 'Shoplex',
-  // countries: () => {
-  //   return [{ code: 'my', name: 'Malaysia' }]
-  // },
-}
-
-const date = {
-  standard: (date, locale) => {
-    const options = { year: 'numeric', month: 'short', day: 'numeric' }
-    return new Date(date).toLocaleDateString(locale, options)
-  },
 }
 
 const utils = {
-  print: (msg) => {
-    window.console.log(msg)
-  },
-  printj: (msg) => {
-    window.console.log(JSON.stringify(msg))
-  },
   getParentRoute: (path) => {
     return path.substr(0, path.lastIndexOf('/'))
   },
-
   // random: () => {
   //   return Math.random().toString(36).slice(2)
   // },
-
   // editMode: (path) => {
   //   return path.split('/').pop() === 'edit'
   // },
@@ -48,6 +30,5 @@ const utils = {
 export default (_, inject) => {
   inject('ui', ui)
   inject('app', app)
-  inject('date', date)
   inject('utils', utils)
 }
