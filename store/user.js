8233fd52defc982593f62cf180cdafa1d7af9081
Raymond Chai <csechuan@gmail.com>
2021-07-10 22:08:10 +0800

    feat(store): split user store into auth and user store

--- store/user.js
+++ store/user.js
@@ -1,143 +1,73 @@
 import * as consola from 'consola'
+import { FieldValue } from '~/plugins/firebase'
+
+const COLLECTION = 'users'
 
 const state = () => ({
-  me: null,
+  user: null,
 })
 
-const getters = {
-  authenticated: () => !!state.me,
-}
+const getters = {}
 
 const mutations = {
-  SET_ME: (state, { authUser }) => {
-    if (authUser) {
-      const { uid, email, emailVerified, displayName, phoneNumber, photoURL, metadata } = authUser
-      state.me = {
-        email,
-        emailVerified,
-        displayName,
-        phoneNumber,
-        firebaseId: uid,
-        photoUrl: photoURL,
-        creationTime: metadata?.creationTime,
-        lastSignInTime: metadata?.lastSignInTime,
-      }
-    }
-  },
-
-  UNSET_ME: (state) => (state.me = null),
+  SET: (state, payload) => (state.user = { ...payload }),
 }
 
 const actions = {
-  async requestPasswordReset({ email }) {
-    try {
-      this.$fire.auth.languageCode = this.$i18n.locale
-      await this.$fire.auth.sendPasswordResetEmail(email)
-    } catch (error) {
-      // auth/invalid-email -> rely on client validation
-      // auth/user-not-found -> dont let requestor knows
-      // the rest (incl. developer errors) -> retry
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
+  async add({ commit }, payload) {
+    payload = { ...payload, object: 'user' }
+
+    await this.$fire.firestore
+      .collection(COLLECTION)
+      .add(payload)
+      .then((doc) => {
+        if (this.app.context.isDev) consola.info('user.js | add | doc', doc)
+        commit('SET', payload)
+        return new Promise((resolve, reject) => resolve(payload))
+      })
+      .catch((error) => {
+        if (this.app.context.isDev) consola.error('user.js | add | error', error)
+        return new Promise((resolve, reject) => reject(error))
+      })
   },
 
-  async confirmPasswordReset({ code, newPassword }) {
-    try {
-      await this.$fire.auth.confirmPasswordReset(code, newPassword)
-    } catch (error) {
-      // auth/expired-action-code -> show & suggest
-      // auth/invalid-action-code -> show & suggest
-      // auth/user-disabled (e.g. acc locked) -> show & suggest
-      // auth/user-not-found -> dont let requestor knows
-      // auth/weak-password (< 6 chars) -> rely on client validation
-      if (this.app.context.isDev) consola.error(error)
-      return error
+  async update({ commit }, docId, payload) {
+    payload = {
+      ...payload,
+      updated: FieldValue.serverTimestamp(),
     }
+
+    await this.$fire.firestore
+      .collection(COLLECTION)
+      .doc(docId)
+      .update(payload)
+      .then(() => {
+        if (this.app.context.isDev) consola.info('user.js | update', 'Update successful')
+        commit('SET', payload)
+        return new Promise((resolve, reject) => resolve())
+      })
+      .catch((error) => {
+        if (this.app.context.isDev) consola.error('user.js | update | error', error)
+        return new Promise((resolve, reject) => reject(error))
+      })
   },
 
-  async signInWithEmailAndPassword({ commit }, { email, password }) {
-    try {
-      const user = await this.$fire.auth.signInWithEmailAndPassword(email, password)
-      commit('SET_ME', user)
-    } catch (error) {
-      // auth/invalid-email -> rely on client validation
-      // auth/user-disabled (e.g. acc locked) -> show & suggest
-      // auth/user-not-found -> dont let requestor knows
-      // auth/wrong-password -> retry
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
-  },
-
-  async registerWithEmailAndPassword({ commit }, { email, password }) {
-    try {
-      const user = await this.$fire.auth.createUserWithEmailAndPassword(email, password)
-      commit('SET_ME', user)
-    } catch (error) {
-      // auth/email-already-in-use -> retry
-      // auth/invalid-email -> rely on client validation
-      // auth/operation-not-allowed -> dev error
-      // auth/weak-password (< 6 chars) -> rely on client validation
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
-  },
-
-  async signOut({ commit }) {
-    try {
-      await this.$fire.auth.signOut()
-      commit('UNSET_ME')
-    } catch (error) {
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
-  },
-
-  async updatePassword({ newPassword }) {
-    try {
-      const user = this.$fire.auth.currentUser
-      await user.updatePassword(newPassword)
-    } catch (error) {
-      // auth/weak-password (< 6 chars) -> rely on client validation
-      // auth/requires-recent-login -> prompt login first
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
-  },
-
-  async requestEmailVerification() {
-    try {
-      const user = this.$fire.auth.currentUser
-      await user.sendEmailVerification()
-    } catch (error) {
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
-  },
-
-  async confirmEmail({ commit }, { code }) {
-    try {
-      await this.$fire.auth.applyActionCode(code)
-      const user = this.$fire.auth.currentUser
-      commit('SET_ME', user)
-    } catch (error) {
-      // auth/expired-action-code -> show & suggest
-      // auth/invalid-action-code -> show & suggest
-      // auth/user-disabled (e.g. acc locked) -> show & suggest
-      // auth/user-not-found -> dont let requestor knows
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
-  },
-
-  onAuthStateChanged({ commit }, { authUser }) {
-    try {
-      authUser ? commit('SET_ME', authUser) : commit('UNSET_ME')
-    } catch (error) {
-      if (this.app.context.isDev) consola.error(error)
-      return error
-    }
+  async get({ commit }, firebaseId) {
+    await this.$fire.firestore
+      .collection(COLLECTION)
+      .where('firebaseId', '==', firebaseId)
+      .limit(1)
+      .get()
+      .then((snapshot) => {
+        if (this.app.context.isDev) consola.info('user.js | get| snapshot', snapshot)
+        const doc = snapshot[0] // expecting one record ONLY
+        commit('SET', { payload: doc })
+        return new Promise((resolve, reject) => resolve(doc?.data))
+      })
+      .catch((error) => {
+        if (this.app.context.isDev) consola.error('user.js | get | error', error)
+        return new Promise((resolve, reject) => reject(error))
+      })
   },
 }
 
