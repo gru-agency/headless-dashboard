2c980d66d2639fec89cab7a2be845130c3833fcc
Raymond Chai <csechuan@gmail.com>
2021-07-12 05:10:58 +0800

    chore: enable firebase emulator

--- nuxt.config.js
+++ nuxt.config.js
@@ -169,8 +169,8 @@ export default {
           subscribeManually: true,
         },
         ssr: false,
-        // emulatorPort: 9099,
-        // emulatorHost: 'http://localhost',
+        emulatorPort: 9099,
+        emulatorHost: 'http://localhost',
       },
       firestore: {
         enablePersistence: { synchronizeTabs: true },
