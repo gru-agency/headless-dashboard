b5d457028edf344b81a248ef8f25f45ca22f986b
Raymond Chai <csechuan@gmail.com>
2021-07-17 03:35:35 +0800

    feat(user): add user session

--- pages/dashboard/settings/user.vue
+++ pages/dashboard/settings/user.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>Redirected from users page</div>
+  <div class="py-10 px-5">
+    <users-session-table></users-session-table>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'User',
+  layout: 'dashboard',
+
+  data() {
+    return {}
+  },
 }
 </script>
