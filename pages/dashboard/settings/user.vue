56f4e958daafc46572de620b2e77141fd26182ce
Raymond Chai <csechuan@gmail.com>
2021-07-18 17:25:23 +0800

    feat(user): add email preference settings for user

--- pages/dashboard/settings/user.vue
+++ pages/dashboard/settings/user.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="py-10 px-5">
     <users-language-card class="mb-10"></users-language-card>
+    <users-email-card class="mb-10"></users-email-card>
     <users-session-table class="mb-10"></users-session-table>
   </div>
 </template>
