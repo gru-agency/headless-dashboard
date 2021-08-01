80d5d4b44a0013cbb0defdea39383ab56aa51a51
Raymond Chai <csechuan@gmail.com>
2021-08-01 20:09:15 +0800

    feat(products): add product info page

---
+++ components/app/products/InfoCard.vue
@@ -0,0 +1,108 @@
+<template>
+  <b-card class="border-0" no-body>
+    <box-header :title-text="product?.name" parent-link :parent-link-text="ui.title">
+      <template #right>
+        <action-menu
+          :primary-key="productId"
+          :edit-link="editLink"
+          delete-hide
+          :no-archive="!product?.active"
+          :no-unarchive="product?.active"
+          @archive="archive"
+          @unarchive="unarchive"
+        ></action-menu>
+      </template>
+    </box-header>
+
+    <b-card-body>
+      <b-row>
+        <b-col lg="6">
+          <b-row tag="dl" class="mb-0">
+            <b-col xl="3" tag="dt"> ID </b-col>
+            <b-col xl="9" tag="dd"> <text-field :text="productId"></text-field> </b-col>
+            <b-col xl="3" tag="dt"> {{ ui.name }} </b-col>
+            <b-col xl="9" tag="dd"> <text-field :text="product?.name"></text-field> </b-col>
+            <b-col xl="3" tag="dt"> {{ ui.status }} </b-col>
+            <b-col xl="9" tag="dd">
+              <tag-field v-if="product?.active" preset="bv-active" variant="success"></tag-field>
+              <tag-field v-if="!product?.active" preset="bv-archive" variant="secondary"></tag-field>
+            </b-col>
+            <b-col xl="3" tag="dt"> {{ ui.created }} </b-col>
+            <b-col xl="9" tag="dd"> <text-field :date="product?.created.toDate()"></text-field></b-col>
+            <b-col xl="3" tag="dt"> {{ ui.updated }} </b-col>
+            <b-col xl="9" tag="dd"> <text-field :date="product?.updated.toDate()"></text-field></b-col>
+          </b-row>
+        </b-col>
+        <b-col lg="6">
+          <b-row tag="dl" class="mb-0">
+            <b-col xl="3" tag="dt"> {{ ui.images }} </b-col>
+            <b-col xl="9" tag="dd">
+              <image-field :images="product?.images" icon-preset="bv-product" max="5"></image-field>
+            </b-col>
+            <b-col xl="3" tag="dt"> {{ ui.description }} </b-col>
+            <b-col xl="9" tag="dd"> <text-field :text="product?.description"></text-field> </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState } from 'vuex'
+
+export default {
+  name: 'InfoCard',
+
+  data() {
+    return {
+      ui: {
+        title: this.$t('modules.products.title'),
+        name: this.$t('general.name'),
+        images: this.$t('general.images'),
+        created: this.$t('general.created'),
+        updated: this.$t('general.updated'),
+        status: this.$t('general.status'),
+        description: this.$t('general.description'),
+      },
+    }
+  },
+
+  computed: {
+    ...mapState('user', ['user']),
+    ...mapGetters('products', ['findByProduct']),
+
+    account() {
+      return this.user?.account.id
+    },
+
+    product() {
+      return this.findByProduct(this.productId, this.account)
+    },
+
+    productId() {
+      return this.$route.params.id
+    },
+
+    editLink() {
+      return this.localePath({ name: 'dashboard-products-id-edit', params: { id: this.product?.id } })
+    },
+  },
+
+  methods: {
+    ...mapActions('products', ['update']),
+
+    async archive() {
+      if (this.account) {
+        await this.update({ documentId: this.product?.id, payload: { account: this.account, active: false } })
+      }
+    },
+
+    async unarchive() {
+      if (this.account) {
+        await this.update({ documentId: this.product?.id, payload: { account: this.account, active: true } })
+      }
+    },
+  },
+}
+</script>
