2c39fa4372e48730ce307facb82bd1a626cb844a
Raymond Chai <csechuan@gmail.com>
2021-08-12 15:48:10 +0800

    feat(component): add FormFeedback component

---
+++ components/lib/bootstrap/FormFeedback.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-form-text v-if="compat">
+    <slot>
+      <span class="text-danger"><icon preset="bv-error" class="mr-1"></icon>{{ msg }}</span>
+    </slot>
+  </b-form-text>
+
+  <b-form-invalid-feedback v-else>
+    <slot>
+      <span><icon preset="bv-error" class="mr-1"></icon>{{ msg }}</span>
+    </slot>
+  </b-form-invalid-feedback>
+</template>
+
+<script>
+export default {
+  name: 'FormFeedback',
+
+  props: {
+    compat: { type: Boolean, default: false },
+    msg: { type: String, default: undefined },
+  },
+}
+</script>
